c2005f5542192400a75b87dda00302133ae18faa
Seu Nome <fernandonetosoares@hotmail.com>
2024-08-12 15:39:56 -0300

    carrinho de compras

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function adicionar(){
+let nomeProduto = document.getElementById('produto');
+let quantidade = document.getElementById('quantidade');
+
+alert(nomeProduto);
+alert(quantidade);
+
+}
+
+function limpar(){
+
+}
